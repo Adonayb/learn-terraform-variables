5553c58b5edc9b83b888e4da3244bb2df30b906d
TEK Student <TEKstudent@vlab.lan>
2025-08-18 13:47:19 -0700

    changed s3 bucket name

--- main.tf
+++ main.tf
@@ -129,5 +129,5 @@ module "ec2_instances" {
 module "s3_bucket" {
   source  = "app.terraform.io/policy-as-code-training/s3-bucket-ab/aws"
   version = "1.0.0"
-  bucket_name = "my-bucket-ab"
+  bucket_name = "my-bucket-ab-0001"
 }
